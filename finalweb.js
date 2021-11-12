e639d6ad2b3ba97c3de62c7ec9640b646b3da2fe
Rv072 <94162846+Rv072@users.noreply.github.com>
2021-11-12 17:03:11 +0530

    Add files via upload

---
+++ finalweb.js
@@ -0,0 +1,16 @@
+function showflight() {
+    let typeofjourney = document.getElementById("typeof").value;
+    let searchflight = document.getElementById("search").value;
+    let origincity = document.getElementById("input").value;
+    let Destinationcity = document.getElementById("Input").value
+
+    if (!origincity) {
+        alert("please see once again")
+return;
+    }
+}
+document.getElementById("type of journey").innerHTML=typeofjourney;
+document.getElementById("searchflight").innerHTML=searchflight;
+document.getElementById("origincity").innerHTMorigincity;
+document.getElementById("destinationcity").innerHTML=destinationcity;
+
